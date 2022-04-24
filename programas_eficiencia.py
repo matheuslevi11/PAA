c1a21d8b5fac882d5e783c912be3e11b49772a2b
Matheus Levi Aidano <matheuslevi.aidano@gmail.com>
2022-04-24 01:45:27 -0300

    Adiciona novas funções para analisar eficiência

---
+++ programas_eficiencia.py
@@ -0,0 +1,34 @@
+def prog1(k, c):
+    if k == 1:
+        print("Oi")
+        c += 1
+        return c
+    else:
+        for i in range(1,k+1):
+            print("Oi")
+            c += 1
+        return prog1(k-1, c)
+
+def cont(n):
+    c = 0
+    for l in range(1, n+1):
+        for i in range(1, n-l+1):
+            for k in range(1, i+l+1):
+                c += 1
+    return c
+
+def asterisco(n):
+    if n > 0:
+        asterisco(n-1)
+        for i in range(n):
+            print('*', end='')
+        asterisco(n-1)
+
+def recorrencia_asterisco(n):
+    if n == 1:
+        return 1
+    return 2 * recorrencia_asterisco(n-1) + n
+
+for i in range(1, 6):
+    asterisco(i)
+    print(recorrencia_asterisco(i))
